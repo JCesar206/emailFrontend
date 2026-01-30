87ba54fbf81b358a755f7469cb5b889ca23e14e8
Julio Cesar <jcesar206@hotmail.com>
2026-01-30 10:53:32 -0600

    Se agrega componente nuevo para mejor manejo de estructura de componentes principales

--- src/layouts/MailLayout.jsx
+++ src/layouts/MailLayout.jsx
@@ -6,8 +6,10 @@ export default function MailLayout({ children }) {
 	return (
 		<div className="min-h-screen flex flex-col bg-gray-100">
 			<Navbar/>
-			<div className="flex flex-1">
-				<Sidebar/>
+			<div className="flex flex-1 overflow-hidden">
+				<aside className="hidden md:block">
+					<Sidebar/>
+				</aside>
 				<main className="flex-1 p-3 md:p-6 overflow-y-auto">
 					{children}
 				</main>
