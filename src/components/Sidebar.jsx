433f52c4ca0ddaaacfe5d37a3c97f7900b6a70c1
Julio Cesar <jcesar206@hotmail.com>
2026-01-30 10:52:35 -0600

    Se mejora diseño y logica del componente Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,7 +3,7 @@ import { Inbox, Send, Trash2, Plus } from "lucide-react";
 export default function Sidebar({ onCompose }) {
 	return (
 		<div className="p-4 space-y-4">
-			<button onClick={onCompose} className="w-full flex items-center gap-2 bg-blue-500 text-white py-2 px-3 rounded-full"><Plus size={18}/> Redactar </button>
+			<button onClick={onCompose} className="w-full flex items-center gap-2 bg-blue-500 text-white py-2 px-3 rounded-full cursor-pointer"><Plus size={18}/> Redactar </button>
 
 			<nav className="space-y-3 text-sm">
 				<p className="flex gap-2 items-center font-semibold"><Inbox size={16}/> Inbox </p>
