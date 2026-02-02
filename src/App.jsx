5a001c852a626da1d05405e316b14329631787d4
Julio Cesar <jcesar206@hotmail.com>
2026-02-02 11:30:56 -0600

    App.js: Se actualiza para manejo de cambio de tema global

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import AppRouter from "./routes/AppRouter";
+import { AuthProvider } from "./auth/AuthContext";
+import { ThemeProvider } from "./context/ThemeContext";
 import "./App.css";
 
 function App() {
-  return <AppRouter/>
+  return (
+    <ThemeProvider>
+      <AuthProvider>
+        <AppRouter/>
+      </AuthProvider>
+    </ThemeProvider>
+  );
 }
 
 export default App;
