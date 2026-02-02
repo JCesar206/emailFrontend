3291a8d17493cc85c76a77c7c4c8c5befa560221
Julio Cesar <jcesar206@hotmail.com>
2026-02-02 17:13:59 -0600

    en.js: Se agrega diccionario de ingles para app bilingüe

---
+++ src/i18n/en.js
@@ -0,0 +1,41 @@
+export default {
+  navbar: {
+    inbox: "Inbox",
+    sent: "Sent",
+    logout: "Logout",
+		theme: "Theme",
+		language: "Language"
+  },
+  sidebar: {
+    inbox: "Inbox",
+    sent: "Sent",
+    drafts: "Drafts",
+    trash: "Trash",
+    compose: "Compose"
+  },
+  common: {
+    back: "Back",
+    cancel: "Cancel"
+  },
+  compose: {
+    new: "New",
+    to: "To",
+    subject: "Subject",
+    message: "Message",
+    send: "Send"
+  },
+  mail: {
+    from: "From",
+    to: "To",
+    subject: "Subject",
+    empty: "No emails",
+    attachments: "Attachments",
+    select: "Select an email"
+  },
+  inbox: {
+    title: "Inbox"
+  },
+	footer: {
+		rights: "My Personal Blog"
+	}
+};
