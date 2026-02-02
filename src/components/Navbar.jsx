b78f38916c04f0aa1ae95e719a26f6c79b2ee4af
Julio Cesar <jcesar206@hotmail.com>
2026-02-02 11:33:24 -0600

    Navbar.jsx: Se actualiza manejo de cambio de tema global

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,42 +1,30 @@
-import { LogOut, Search, Sun, Moon } from "lucide-react";
-import { useContext, useState } from "react";
+import { LogOut, Sun, Moon } from "lucide-react";
+import { useContext } from "react";
 import { AuthContext } from "../auth/AuthContext";
+import { useTheme } from "../context/ThemeContext";
 
 export default function Navbar() {
   const { logout } = useContext(AuthContext);
-  const [dark, setDark] = useState(false);
-
-  const toggleTheme = () => {
-    setDark(!dark);
-    document.documentElement.classList.toggle("dark");
-  };
+  const { theme, toggleTheme } = useTheme();
 
   return (
-    <header className="sticky top-0 z-50 bg-white dark:bg-gray-900 border-b">
-      <div className="flex items-center justify-between px-4 py-2 gap-3">
-        
-        {/* Logo */}
-        <h1 className="font-bold text-lg text-blue-600"> MailApp</h1>
+    <header className="sticky top-0 z-50 bg-white dark:bg-gray-900 border-b px-4 h-14 flex items-center justify-between">
+      <h1 className="font-bold text-lg text-gray-800 dark:text-white">
+        Mail App
+      </h1>
 
-        {/* Search */}
-        <div className="hidden md:flex flex-1 max-w-md items-center bg-gray-100 dark:bg-gray-800 rounded px-2">
-          <Search size={18} className="text-gray-500" />
-          <input type="text" placeholder="Buscar correo"
-            className="bg-transparent outline-none px-2 py-1 w-full text-sm"
-          />
-        </div>
+      <div className="flex items-center gap-3">
+        <button onClick={toggleTheme}
+          className="p-2 rounded hover:text-blue-500 dark:hover:bg-white dark:text-white dark:hover:text-blue-800 cursor-pointer"
+        >
+          {theme === "dark" ? <Sun size={18} /> : <Moon size={18} />}
+        </button>
 
-        {/* Actions */}
-        <div className="flex items-center gap-3">
-          <button onClick={toggleTheme} className="p-2 rounded hover:bg-gray-200 dark:hover:bg-gray-700 cursor-pointer">
-            {dark ? <Sun size={18} /> : <Moon size={18} />}
-          </button>
-
-          <button onClick={logout} className="flex items-center gap-1 text-sm text-red-600 hover:text-red-800 cursor-pointer">
-            <LogOut size={18} />
-            <span className="hidden sm:inline">Salir</span>
-          </button>
-        </div>
+        <button onClick={logout}
+          className="p-2 rounded hover:text-red-500 dark:hover:bg-white dark:text-white dark:hover:text-red-800 cursor-pointer"
+        >
+          <LogOut size={18} />
+        </button>
       </div>
     </header>
   );
