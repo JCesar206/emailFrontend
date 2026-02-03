9dc7e6ba71a28e61506d102df67c53a3eeaf0422
Julio Cesar <jcesar206@hotmail.com>
2026-02-03 13:13:32 -0600

    MailList.jsx: Se actualiza diseño y manejo de idioma y tema del componente

--- src/components/MailList.jsx
+++ src/components/MailList.jsx
@@ -1,12 +1,18 @@
 import { useState, useEffect } from "react";
 import api from "../api/axios.js";
+import { useLanguage } from "../context/LanguageContext.jsx";
 
 export default function MailList({ onSelect }) {
   const [mails, setMails] = useState([]);
+	const { t } = useLanguage();
 
   useEffect(() => {
     api.get("/mail").then(res => setMails(res.data));
   }, []);
+
+		if (!mails.length) {
+		return <p>{t("mail.empty")}</p>
+	}
 
 	return (
 		<div className="overflow-y-auto h-full">
